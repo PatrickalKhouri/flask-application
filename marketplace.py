fb168ea2f13a2748fd8b004581a6651fe1c93b2e
Patrick Khouri <patrick.darya@gmail.com>
2020-11-21 15:45:30 -0300

    Added some dummy data and showed them in the home page

--- marketplace.py
+++ marketplace.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+products = [
+    {
+       'reference': 'NK 212',
+       'country': 'India',
+       'type':'kilim',
+       'height': 2,
+       'width': 1.5,
+       'm2': 3,
+       'quantity': 5,
+       'price': 1000,
+       'description': 'Kilim Indian - Red',
+       'Images': "...",
+       'created_at':'November 20, 2019'
+    },
+    {
+       'reference': 'HZ 0156',
+       'country': 'Afganistan',
+       'type':'carpet',
+       'height': 5,
+       'width': 4,
+       'm2': 20,
+       'quantity': 3,
+       'price': 12000,
+       'description': 'Large colorfull carpet',
+       'Images': "...",
+       'created_at':'November 10, 2019'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', products=products)
 
 @app.route('/about')
 def about():
-    return "<h1>About</h1>"
+    return render_template('about.html')
